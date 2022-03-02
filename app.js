e8f40d3d44ab2877e178cae38d420973fe692bfd
kevinfarukh <kevinfarukh@gmail.com>
2022-03-02 16:00:08 -0500

    port

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Servidor en el puerto 3000")
 })
